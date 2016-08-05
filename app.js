76e5c926b4293865ac6d62ff5fe3e4355917bda1
chenlei <leochen.shanghai@qq.com>
2016-08-05 14:05:44 +0800

    xlsx import column number limit extent to BS

--- app.js
+++ app.js
@@ -1485,7 +1485,17 @@ aclHandler.registerWait(function(acl){
             "F","G","H","I","J",
             "K","L","M","N","O",
             "P","Q","R","S","T",
-            "U","V","W","X","Y","Z"];
+            "U","V","W","X","Y","Z",
+            "AA","AB","AC","AD","AE",
+            "AF","AG","AH","AI","AJ",
+            "AK","AL","AM","AN","AO",
+            "AP","AQ","AR","AS","AT",
+            "AU","AV","AW","AX","AY","AZ",
+            "BA","BB","BC","BD","BE",
+            "BF","BG","BH","BI","BJ",
+            "BK","BL","BM","BN","BO",
+            "BP","BQ","BR","BS"
+        ];
 
         if(file){
             var fextenstionarray = file.split(".");
